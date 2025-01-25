5f4cf3526e1ba115880d8e10fc599aa9fe1075cb
MrBocch <jorgealberto1436@gmail.com>
2025-01-24 18:47:29 -0600

    Leetle #24 1/6 0:04 [2 lines]
    🟩🟩🟩🟩🟩🟩
    https://leetle.app

---
+++ d24.py
@@ -0,0 +1,8 @@
+# O(n) solution
+# if min function goes thrue n for n 
+
+# if its a sorted array, one could simple binary search
+# but its not just a sorted array, its a rotated arrray
+# you cant simply binary search thrue it. Maybe you can do something clever
+def solve(nums):
+  return min(nums)
